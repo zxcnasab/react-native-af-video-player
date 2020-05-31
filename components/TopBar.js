9593715cd756e56a2a2d7efdf2e3f46be3f5436c
Neil John Basan <basaneiljohn@gmail.com>
2020-05-31 23:48:58 +0800

    Replace react-native-linear-gradient with expo-linear-gradient

--- components/TopBar.js
+++ components/TopBar.js
@@ -8,7 +8,7 @@ import {
   Image
 } from 'react-native'
 
-import LinearGradient from 'react-native-linear-gradient'
+import LinearGradient from 'expo-linear-gradient'
 import { ToggleIcon } from './'
 import { checkSource } from './utils'
 
